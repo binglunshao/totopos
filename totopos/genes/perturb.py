3cb73c42b0e4880730b819b0c68cac849d22817d
manu <manuflores@caltech.edu>
2024-10-18 14:17:09 -0700

    bugfix in perturb method--needed k+1 skeleton for k homology..

--- totopos/genes/perturb.py
+++ totopos/genes/perturb.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def topological_gene_scores_via_perturbation(
     data:np.ndarray, max_radius:float=2.0, n_threads:int=2,
-    max_hom_dim:int=1, n_topo_feats:int=1, verbose:bool = False
+    hom_dim:int=1, n_topo_feats:int=1, verbose:bool = False
     )->np.ndarray:
     """
     Returns gene scores via a perturbation approach. 
@@ -23,7 +23,7 @@ def topological_gene_scores_via_perturbation(
     if verbose:print("Calculating Vietoris-Rips filtration...")
     vr_filtration = oin.diff.vietoris_rips_pwdists(
         dists, 
-        max_dim=max_hom_dim+1,# need the k+1 skeleton for k-homology
+        max_dim=hom_dim+1,# need the k+1 skeleton for k-homology
         max_radius=max_dist/2, 
         n_threads=n_threads
     )
@@ -34,11 +34,10 @@ def topological_gene_scores_via_perturbation(
     if verbose:print("Computing persistent homology...")
     dgm = top_opt.compute_diagram(include_inf_points=False)
     if verbose:print("Finished persistent homology calculation.")
-
-    eps = top_opt.get_nth_persistence(max_hom_dim, n_topo_feats)
+    eps = top_opt.get_nth_persistence(hom_dim, n_topo_feats)
     
     if verbose:print("Calculating gene scores...")
-    indices, values = top_opt.simplify(eps, oin.DenoiseStrategy.BirthBirth, max_hom_dim)
+    indices, values = top_opt.simplify(eps, oin.DenoiseStrategy.BirthBirth, hom_dim)
     critical_sets = top_opt.singletons(indices, values)
     crit_indices, crit_values = top_opt.combine_loss(critical_sets, oin.ConflictStrategy.Max)
     crit_indices = np.array(crit_indices, dtype=np.int32)
@@ -47,4 +46,4 @@ def topological_gene_scores_via_perturbation(
     top_loss.backward()
     if verbose:print("Finished gene score calculation succesfully.")
 
-    return pts.grad.abs().sum(0).numpy(), [dgm[i] for i in range(max_hom_dim+1)]
+    return pts.grad.abs().sum(0).numpy(), [dgm[i] for i in range(hom_dim+1)]
