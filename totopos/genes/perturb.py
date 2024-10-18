2bcd2a6951b114e031753f7abec5dc6affcc546c
manu <manuflores@caltech.edu>
2024-10-18 12:56:29 -0700

    added print messaghes in perturb method

--- totopos/genes/perturb.py
+++ totopos/genes/perturb.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def topological_gene_scores_via_perturbation(
     data:np.ndarray, max_radius:float=2.0, n_threads:int=2,
-    max_hom_dim:int=1, n_topo_feats:int=1,
+    max_hom_dim:int=1, n_topo_feats:int=1, verbose:bool = False
     )->np.ndarray:
     """
     Returns gene scores via a perturbation approach. 
@@ -19,13 +19,17 @@ def topological_gene_scores_via_perturbation(
     epsilon = 1e-8
     sq_dists = torch.sum((pts1 - pts2) ** 2, dim=2)
     dists = torch.sqrt(sq_dists + epsilon)
-
+    if verbose:print("Calculating Vietoris-Rips filtration...")
     vr_filtration = oin.diff.vietoris_rips_pwdists(
         dists, max_dim=max_hom_dim, max_radius=max_radius, n_threads=n_threads
     )
+    if verbose:print("Finished filtration.")
     top_opt = oin.diff.TopologyOptimizer(vr_filtration)
+    if verbose:print("Computing persistent homology...")
     dgm = top_opt.compute_diagram(include_inf_points=False)
+    if verbose:print("Finished persistent homology calculation.")
     eps = top_opt.get_nth_persistence(max_hom_dim, n_topo_feats)
+    if verbose:print("Calculating gene scores...")
     indices, values = top_opt.simplify(eps, oin.DenoiseStrategy.BirthBirth, max_hom_dim)
     critical_sets = top_opt.singletons(indices, values)
     crit_indices, crit_values = top_opt.combine_loss(critical_sets, oin.ConflictStrategy.Max)
@@ -33,5 +37,5 @@ def topological_gene_scores_via_perturbation(
     crit_values = torch.Tensor(crit_values)
     top_loss = torch.mean((vr_filtration.values[crit_indices] - crit_values) ** 2)
     top_loss.backward()
-
+    if verbose:print("Finished gene score calculation succesfully.")
     return pts.grad.abs().sum(0).numpy()
