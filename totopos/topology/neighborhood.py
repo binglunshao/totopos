63e933a7a164da7daf8c226303cc418bc3f36d19
manu <manuflores@caltech.edu>
2024-10-18 11:56:10 -0700

    added simplicial topology module

---
+++ totopos/topology/neighborhood.py
@@ -0,0 +1,59 @@
+"""
+Estimate persistent homology lifetimes noise floor. 
+"""
+import numpy as np 
+from tqdm import tqdm
+from ripser import ripser
+from sklearn.cluster import MiniBatchKMeans as kmeans
+
+def get_lifetimes(dgm):
+    death_radius, birth_radius = dgm[:, 1], dgm[:, 0]
+    return death_radius - birth_radius
+    
+def get_largest_lifetime_from_diagram(dgm): 
+    lifetimes = get_lifetimes(dgm)
+    return sorted(lifetimes, reverse=True)[0]
+
+def largest_neighborhood_lifetime(
+    data, 
+    n_clusters=20,
+    ph_dim=1
+): 
+    """Returns the noise floor $\alpha$ defined as the largest PH lifetime across k neighborhoods using kmeans clustering.
+
+    Params
+    ------
+    data (np.ndarray)
+        Point cloud on which to compute the persistent homology. 
+
+    n_clusters(int)
+        Parameter k for Kmeans clustering. 
+    
+    ph_dim (int)
+        Dimension for PH to focus on.
+    """
+    
+    km = kmeans(n_clusters=n_clusters)
+    km.fit(data)
+    labels=km.predict(data)
+    largest_nbd_lifetime=0
+    neighborhood_lifetimes = []
+    for i in tqdm(range(n_clusters),desc="Estimating PH noise floor using Kmeans neighborhoods"):
+        persistence_diagram = ripser(data[labels==i],maxdim=ph_dim)["dgms"][ph_dim]
+        neighborhood_lifetimes.extend(get_lifetimes(persistence_diagram))
+        if len(persistence_diagram)>0:
+            largest_lt_clus = get_largest_lifetime_from_diagram(persistence_diagram)
+            if largest_lt_clus > largest_nbd_lifetime:
+                largest_nbd_lifetime = largest_lt_clus
+    return largest_nbd_lifetime, neighborhood_lifetimes
+
+
+def neighborhood_subsample(data, n_clusters): 
+    km = kmeans(n_clusters=n_clusters)
+    km.fit(data)
+    labels=km.predict(data)
+    # Use numpy.unique to get the first occurrence of each label
+    _, representative_indices = np.unique(labels, return_index=True)
+    
+    # Return the indices as a list, one for each cluster
+    return labels, representative_indices#.tolist()
---
+++ totopos/topology/simplicial.py
@@ -0,0 +1,81 @@
+"""Designed for computing simplicial 1-cohomology representatives using Scoccola et al. Toroidal Coordinates algorithm.
+"""
+from dreimac import ToroidalCoords
+import warnings
+from .neighborhood import get_lifetimes, get_largest_lifetime_from_diagram, largest_neighborhood_lifetime
+
+class SimplicialTopology(): 
+    def __init__(self, data, n_pts = None, maxdim=1):
+        self.data = data
+        self.n_pts = n_pts 
+        self.maxdim = maxdim
+    
+    def compute_persistent_cohomology(self, verbose=False):
+        """Computes Persistent Cohomology using Bauer's ripser algorithm."""
+        self.ph = ToroidalCoords(
+            self.data, n_landmarks=self.n_pts, maxdim=self.maxdim, prime=47, verbose=verbose
+        )
+
+    def estimate_neighborhood_threshold(self, ph_dim = 1, neighborhood_size = 300): 
+        """Estimates the homology lifetime "noise floor" defined as the largest PH lifetime 
+        across k neighborhoods using kmeans clustering."""
+
+        assert ph_dim >= len(self.ph.dgms_)-1, "Desired homology dimension was not computed, please reinstaintate object."
+        dgm = self.ph.dgms_[ph_dim]
+        lifetimes = get_lifetimes(dgm)
+        largest_nbd_lifetime, neighborhood_lifetimes = largest_neighborhood_lifetime(
+            self.data, n_clusters=len(self.data)//neighborhood_size, ph_dim = ph_dim
+        )
+        largest_nbd_lifetime *= 1.1 
+        self.neighborhood_lifetime_threshold = {ph_dim:largest_nbd_lifetime}
+        n_prominent_feats_ = sum(lifetimes >= largest_nbd_lifetime)
+        self.n_prominent_feats = {ph_dim: n_prominent_feats_}
+
+    def get_harmonic_reps(self, perc = .1, check_consistency = False, n_classes = None):
+        """Computes harmonic representatives using Scoccola et al. Toroidal Coordinates algorithm.
+        """
+        assert 1 in self.n_prominent_feats.keys(), f"Please run estimate_neighborhood_threshold() for dim 1."
+        n_classes = self.n_prominent_feats[1] if n_classes is None else n_classes
+        self.toroidal_coords = self.ph.get_coordinates(
+            perc=perc, 
+            cocycle_idxs = range(n_classes), 
+            standard_range=False, 
+            check_cocycle_condition=True
+        ).T
+
+        if check_consistency: 
+            self.toroidal_coords_consistency_check()
+
+    def fit(self,verbose=False):
+        self.compute_persistent_cohomology(verbose=verbose)
+        self.estimate_neighborhood_threshold()
+        self.get_harmonic_reps()
+    
+    def fit_transform(self, data):
+        self.fit()
+        return self.toroidal_coords
+    
+    def transform(self, data): 
+        return self.toroidal_coords
+
+    def get_harmonic_rep_indicators(self): 
+        return self.toroidal_coords > np.pi
+
+    def is_invalid_harmonic_rep(self, index, thresh = .5): 
+        fraction_nz_pts = sum(self.toroidal_coords[:, i] > np.pi)/len(self.data)
+        return fraction_nz_pts > thresh
+
+    def correct_for_one_inconsistent_toroidal_coord(self, coords, invalid_idx):
+        indicators = get_harmonic_rep_indicators()
+        indicators[:, invalid_idx] = indicators.sum(1) % 2
+        self.toroidal_coords[:, invalid_idx] = self.toroidal_coords[:, invalid_idx][indicators[:, invalid_idx]]
+        return coords
+
+    def consistency_check(self):
+        invalid = [is_invalid_harmonic_rep(self.toroidal_coords[:, i]) for i in range(self.n_prominent_feats)]
+        if np.sum(invalid)>0:
+            if np.sum(invalid)==1:
+                invalid_idx = np.flatnonzero(invalid)[0]
+                self.toroidal_coords = self.correct_for_one_inconsistent_toroidal_coord(self.toroidal_coords, invalid_idx)
+            else:
+                warnings.warn(f"There are {np.sum(invalid)}/{self.n_prominent_feats} invalid harmonic reps, could not correct them.")
