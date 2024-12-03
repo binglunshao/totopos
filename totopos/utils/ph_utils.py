c68fe7e247302b085271fff5cd71802c16ef202f
Manu Flores <emanuelflrsb@hotmail.com>
2024-12-03 03:01:54 -0800

    changed min_enclosing_radius func

--- totopos/utils/ph_utils.py
+++ totopos/utils/ph_utils.py
@@ -1,11 +1,11 @@
 import numpy as np 
 
-def min_enclosing_radius(D):
-    return D.max(1).min()
+def min_enclosing_radius_torch(D):
+    return D.max(1).values.min()
 
-def min_enclosing_radius_subset(D, subset_size=1000):
+def min_enclosing_radius_subset_torch(D, subset_size=1000):
     if D.shape[0]>subset_size:
         randixs=np.random.choice(D.shape[0], size=subset_size,replace=False)
-        return D[randixs].max(1).min()
+        return D[randixs].max(1).values.min()
     else:
-        return D.max(1).min()
+        return D.max(1).values.min()
