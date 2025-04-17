1226feca5fa9d32c1bb352eb358efd6e7d887d8a
manu <manuflores@caltech.edu>
2025-04-17 14:29:37 -0700

    added fps func

--- totopos/utils/ph_utils.py
+++ totopos/utils/ph_utils.py
@@ -1,4 +1,5 @@
 import numpy as np 
+from sklearn.metrics import pairwise_distances
 
 def min_enclosing_radius_torch(D):
     return D.max(1).values.min()
@@ -13,3 +14,25 @@ def min_enclosing_radius_subset_torch(D, subset_size=1000):
 def get_lifetimes(dgm):
     """Returns lifetimes given a persistence diagram with format (n,2) array of birth-death pairs"""
     return dgm[:, 1]- dgm[:, 0]
+
+def greedy_farthest_point_sampling(X, n_points=1000,ix_start=None): 
+    """
+    Returns
+    -------
+    ixs (np.ndarray)
+        Indices of the selected points.
+    hausdorff_distance (float)
+    """
+    ixs=np.zeros(n_points,dtype=int)
+    ixs[0]=ix_start if ix_start is not None else np.random.randint(0,high=len(X))
+    
+    # D keeps track of min distance to any selected point so far
+    D = pairwise_distances(X, X[ixs[0], np.newaxis]).flatten()
+
+    for i in range(1, n_points):
+        ixs[i] = np.argmax(D)  # Select the farthest point from current subset
+        d = pairwise_distances(X, X[ixs[i], np.newaxis]).flatten()
+        D = np.minimum(D, d)   # Update distances to the closest selected point
+
+    hausdorff_distance = D.max()  # max over all x ∈ X of min_s∈S ||x - s||
+    return ixs, hausdorff_distance
