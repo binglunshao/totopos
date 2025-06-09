083390ff06c41a51f247a51251df7b05834def96
manu <manuflores@caltech.edu>
2025-06-09 01:55:24 -0700

    removed totopos class from perturb_ripser module, added first vers of iterative module

--- totopos/genes/perturb_ripser.py
+++ totopos/genes/perturb_ripser.py
@@ -41,12 +41,12 @@ def topological_scores_perturbation_torch_ripser(
             thresh=np.inf if max_distance is None else max_distance*1.1
         )
 
-    topological_ranking_scores, gradients = compute_topological_scores(data, pcs, ph, ix_top_class)
+    topological_ranking_scores, gradients = compute_topological_scores_perturbation(data, pcs, ph, ix_top_class)
 
     return topological_ranking_scores, gradients
 
 
-def compute_topological_scores(data, pcs, ph, ix_top_class: int = 1):
+def compute_topological_scores_perturbation(data, pcs, ph, ix_top_class: int = 1):
     """
     Compute topological ranking scores based on persistent omology.
     Identifies the most persistent cohomology class (by default, the top class) from the persistence diagram,
@@ -83,78 +83,3 @@ def compute_topological_scores(data, pcs, ph, ix_top_class: int = 1):
     gradients = data.grad
     topological_ranking_scores = gradients.norm(dim=0).numpy()
     return topological_ranking_scores, gradients
-
-
-class TopoGenes():
-    def __init__(self, adata: ad.AnnData, ph: dict = None, n_pcs: int = 20, max_distance: float = None, verbose: bool = False):
-        """
-        Initialize the topoGenes class.
-
-        Parameters:
-        - adata (AnnData): Annotated data matrix.
-        - n_pcs (int, optional): Number of principal components to use. Defaults to 20.
-        - max_distance (float, optional): Maximum distance threshold for persistence computation. Defaults to None.
-        """
-        data = adata.X.A
-        pts = torch.Tensor(data)
-        pts.requires_grad_(True);
-        self.data = pts
-        self.n_pcs = n_pcs
-        self.max_distance = max_distance
-        self.compute_pca()
-        if ph==None:
-            if verbose: print("Computing persistent homology...")
-            self.ph = ripser(
-                self.pcs.detach().numpy(),
-                do_cocycles=True, 
-                thresh=np.inf if max_distance is None else max_distance*1.1
-            )
-        
-        self.cocycles = self.ph["cocycles"]
-        self.dgms = self.ph["dgms"]
-    
-    def compute_pca(self, transform = False):
-        """
-        Compute PCA on the data using torch.
-        """
-        self.pcs = randomized_pca_torch(self.data, self.n_pcs)
-        if transform:
-            return self.pcs
-    
-    def compute_topological_scores(self, ix_top_class: int = 1):
-        """
-        Compute topological ranking scores based on persistent omology.
-        Identifies the most persistent cohomology class (by default, the top class) from the persistence diagram,
-        extracts its representative cocycle edges, and computes a topological loss based on the squared distances
-        between critical points in the principal component space. The loss is backpropagated to obtain gradients,
-        which are then used to rank features by their topological relevance.
-        Params
-        ------ 
-            ix_top_class (int, optional): Index (from the top) of the persistent cohomology class to analyze. 
-                Defaults to 1 (the most persistent class).
-        Returns
-        -------
-            Tuple[np.ndarray, torch.Tensor]: 
-                - topological_ranking_scores: Feature-wise ranking scores derived from gradient norms.
-                - gradients: Raw gradients of the data with respect to the topological loss.
-        """
-
-        cocycles, dgms = self.cocycles, self.dgms
-
-        
-        lifetimes = get_lifetimes(dgms[1])
-        ix_cohom_class = np.argsort(lifetimes)[-ix_top_class]
-        cocycle_edges_largest_hom_class = cocycles[1][ix_cohom_class][:, :2] # first two entries are edges 
-        cocycle_edges_largest_hom_class = cocycle_edges_largest_hom_class[:, ::-1] # get edges in lexicographic order 
-        death_time = dgms[1][ix_cohom_class][1]
-
-        crit_edges_idx_x, crit_edges_idx_y = cocycle_edges_largest_hom_class.T
-
-        filt_values = torch.sum((self.pcs[crit_edges_idx_x, :] - self.pcs[crit_edges_idx_y, :])**2, axis=1) # distance of largest edges in critical simplices
-        target_crit_values = torch.repeat_interleave(torch.Tensor([death_time]), repeats=len(cocycle_edges_largest_hom_class))
-        topo_loss = torch.norm(target_crit_values - filt_values)
-        topo_loss.backward(retain_graph=True) # to run multiple times, works since grads are computed w.r.t. to different slices of data
-
-        gradients = self.data.grad
-        topological_ranking_scores = gradients.norm(dim=0).numpy()
-        return topological_ranking_scores, gradients
